b1543a3d5efbaf3ff4e8440127b981b815924892
Michal Proszek <michal.proszek@gmail.com>
2015-02-23 14:18:30 +0100

    Automatize .crx generation with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,50 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+
+    grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-crx');
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
 
-        /*uglify: {
+        pkg: grunt.file.readJSON('package.json'),
+        manifest: grunt.file.readJSON('app/manifest.json'),
 
-         css: {
-         files: {
+        shell: {
+            bower: {
+                command: "./bower install"
+            },
+            bowerInstaller: {
+                command: "./bower-installer"
+            }
+        },
 
-         }
-         }
-
-         },*/
+        crx: {
+            staging: {
+                "src": "app/",
+                "dest": "dist/staging/<%= pkg.name %>-<%= manifest.version %>-dev.crx",
+                "baseURL": "https://github.com/poxip/xkcd-today",
+                "filename": "",
+                "exclude": [
+                    ".git", ".svn", "*.pem"
+                ],
+                "privateKey": "~/.ssh/chrome-apps.pem"
+            },
+            production: {
+                "src": "app/",
+                "dest": "dist/production/<%= pkg.name %>-<%= manifest.version %>.crx",
+                "baseURL": "https://github.com/poxip/xkcd-today",
+                "exclude": [
+                    ".git", ".svn", "dev/**", "*.pem"
+                ],
+                "options": {
+                    "maxBuffer": 3000 * 1024 //build extension with a weight up to 3MB
+                }
+            }
+        }
     });
 
-    // grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('install', ['shell:bower', 'shell:bowerInstaller']);
+    grunt.registerTask('build', ['crx']);
+
+    grunt.registerTask('default', ['install', 'build']);
 };
